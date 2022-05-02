ab6b9471bd2a10d7c1941285b8c79dbc2016a17a
bilalcaliskan <bilalcaliskan@protonmail.com>
2022-05-02 08:06:23 +0300

    chore: remove wrong project naming

--- internal/options/options.go
+++ internal/options/options.go
@@ -2,7 +2,7 @@ package options
 
 var s3SubstringFinderOptions = &S3SubstringFinderOptions{}
 
-// OreillyTrialOptions contains frequent command line and application options.
+// S3SubstringFinderOptions contains frequent command line and application options.
 type S3SubstringFinderOptions struct {
 	// AccessKey is the access key credentials for accessing AWS over client
 	AccessKey string
@@ -16,7 +16,7 @@ type S3SubstringFinderOptions struct {
 	Substring string
 }
 
-// GetOreillyTrialOptions returns the pointer of OreillyTrialOptions
+// GetS3SubstringFinderOptions returns the pointer of S3SubstringFinderOptions
 func GetS3SubstringFinderOptions() *S3SubstringFinderOptions {
 	return s3SubstringFinderOptions
 }
